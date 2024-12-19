6381b762397fe0b89cf7844c90aab0d7f6470607
liufeihua <1002219331@qq.com>
2024-12-19 14:26:58 +0800

    更新vo和添加route

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
 use std::env;
 
-use axum::{middleware as md, routing::{get, post}, Router};
-use diesel::MysqlConnection;
+use crate::middleware::auth::auth;
+use crate::route::system::sys_menu_route::build_sys_menu_route;
+use crate::route::system::sys_role_route::build_sys_role_route;
+use crate::route::system::sys_user_route::build_sys_user_route;
+use axum::{middleware as md, Router};
 use diesel::r2d2::{self, ConnectionManager};
+use diesel::MysqlConnection;
 use dotenvy::dotenv;
 use once_cell::sync::Lazy;
-use handler::system::{menu_handler, role_handler, user_handler};
-use crate::middleware::auth::auth;
 
-pub mod model;
-pub mod vo;
-pub mod handler;
-pub mod utils;
-pub mod schema;
-pub mod middleware;
 pub mod common;
+pub mod handler;
+pub mod middleware;
+pub mod model;
+pub mod route;
+pub mod schema;
+pub mod utils;
+pub mod vo;
 
 type DbPool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
 
 pub static RB: Lazy<DbPool> = Lazy::new(|| {
     let database_url = env::var("database_url").expect("database_url must be set");
     let manager = ConnectionManager::<MysqlConnection>::new(database_url);
-    r2d2::Pool::builder().build(manager).expect("Failed to create pool.")
+    r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.")
 });
 
 #[tokio::main]
@@ -29,30 +34,15 @@ async fn main() {
     dotenv().ok();
     log4rs::init_file("src/config/log4rs.yaml", Default::default()).unwrap();
 
-
-    let app = Router::new()
-        .nest("/api", Router::new()
-            .route("/login", post(user_handler::login))
-            .route("/query_user_role", post(user_handler::query_user_role))
-            .route("/update_user_role", post(user_handler::update_user_role))
-            .route("/query_user_menu", get(user_handler::query_user_menu))
-            .route("/user_list", post(user_handler::query_user_list))
-            .route("/user_save", post(user_handler::add_user))
-            .route("/user_delete", post(user_handler::delete_user))
-            .route("/user_update", post(user_handler::update_user))
-            .route("/update_user_password", post(user_handler::update_user_password))
-            .route("/query_role_menu", post(role_handler::query_role_menu))
-            .route("/update_role_menu", post(role_handler::update_role_menu))
-            .route("/role_list", post(role_handler::query_role_list))
-            .route("/role_save", post(role_handler::add_role))
-            .route("/role_delete", post(role_handler::delete_role))
-            .route("/role_update", post(role_handler::update_role))
-            .route("/menu_list", post(menu_handler::query_menu_list))
-            .route("/menu_save", post(menu_handler::add_menu))
-            .route("/menu_delete", post(menu_handler::delete_menu))
-            .route("/menu_update", post(menu_handler::update_menu))
-            .route_layer(md::from_fn(auth)));
-
+    let app = Router::new().nest(
+        "/api",
+        Router::new()
+            .merge(build_sys_user_route())
+            .merge(build_sys_role_route())
+            .merge(build_sys_menu_route())
+            .route_layer(md::from_fn(auth))
+            .route_layer(md::from_fn(auth)),
+    );
 
     // axum 0.6.x
     // let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
@@ -66,4 +56,3 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
-
---
+++ src/route/system/sys_menu_route.rs
@@ -0,0 +1,30 @@
+use crate::handler::system::sys_menu_handler;
+use crate::AppState;
+use axum::routing::post;
+use axum::Router;
+use std::sync::Arc;
+
+/*
+ *构建菜单信息路由
+ *author：刘飞华
+ *date：2024/12/12 17:04:49
+ */
+pub fn build_sys_menu_route() -> Router<Arc<AppState>> {
+    Router::new()
+        .route("/add_menu", post(sys_menu_handler::add_sys_menu))
+        .route("/delete_menu", post(sys_menu_handler::delete_sys_menu))
+        .route("/update_menu", post(sys_menu_handler::update_sys_menu))
+        .route(
+            "/update_menu_status",
+            post(sys_menu_handler::update_sys_menu_status),
+        )
+        .route(
+            "/query_menu_detail",
+            post(sys_menu_handler::query_sys_menu_detail),
+        )
+        .route(
+            "/query_menu_list",
+            post(sys_menu_handler::query_sys_menu_list),
+        )
+    //记得在main.rs中添加路由build_sys_menu_route()
+}
